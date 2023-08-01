9a055f3349e8e2834d42cb5b537cf67f048dc0fb
TheEnumbin <enumbin@gmail.com>
2023-08-01 07:35:51 +0600

    Working on sync

--- controllers/admin/AdminAjaxOmniverseController.php
+++ controllers/admin/AdminAjaxOmniverseController.php
@@ -137,4 +137,118 @@ class AdminAjaxOmniverseController extends ModuleAdminController
             exit;
         }
     }
+
+    public function ajaxProcessOmniDataSync()
+    {
+        $start = Tools::getValue('start');
+        $context = Context::getContext();
+        $lang_id = $context->language->id;
+        $shop_id = $context->shop->id;
+        // $languages = Language::getLanguages(false);
+        
+        // foreach ($languages as $lang) {
+            
+        // }
+        $products = Product::getProducts($this->context->language->getId(), 0, 10, 'id_product', 'ASC');
+        $insert_q = '';
+        foreach($products as $product){
+           
+            $attributes = Product::getProductAttributesIds($product['id_product']);
+            if(isset($attributes) && !empty($attributes)){
+                // die(__FILE__ . ' : ' . __LINE__);
+            }else{
+                $specific_prices = SpecificPrice::getByProductId($product['id_product']);
+                $price_amount = $product['price'];
+                if(isset($specific_prices) && !empty($specific_prices)){
+
+                    foreach($specific_prices as $specific_price){
+
+                        // Reduction
+                        if ($specific_price['reduction_type'] == 'amount') {
+                            $reduction_amount = $specific_price['reduction'];
+
+                            // if (!$specific_price['id_currency']) {
+                            //     $reduction_amount = Tools::convertPrice($reduction_amount, $id_currency);
+                            // }
+
+                            $specific_price_reduction = $reduction_amount;
+
+                            // Adjust taxes if required
+
+                            // if (!$use_tax && $specific_price['reduction_tax']) {
+                            //     $specific_price_reduction = $product_tax_calculator->removeTaxes($specific_price_reduction);
+                            // }
+                            // if ($use_tax && !$specific_price['reduction_tax']) {
+                            //     $specific_price_reduction = $product_tax_calculator->addTaxes($specific_price_reduction);
+                            // }
+                        } else {
+                            $specific_price_reduction = $price * $specific_price['reduction'];
+                        }
+                        $price_amount -= $specific_price_reduction;
+
+                        $existing = $this->check_existance($product['id_product'], $price_amount, $specific_price['id_product_attribute'], $specific_price['id_country'], $specific_price['id_currency'], $specific_price['id_group']);
+
+                        if (empty($existing)) {
+
+                            if($insert_q != ""){
+                                $insert_q .= ',';
+                            }
+                            $insert_q .= '(' . $product['id_product'] . ',' . $specific_price['id_product_attribute'] . ',' . $specific_price['id_country'] . ',' . $specific_price['id_currency'] . ',' . $specific_price['id_group'] . ',' . $price_amount . ',1,"' . date('Y-m-d') . '",' . $shop_id . ',' . $lang_id . ')';
+                        }
+                    }
+
+                }else{
+                    $price_amount = Product::getPriceStatic(
+                        (int) $product['id_product'],
+                        false
+                    );
+                    $existing = $this->check_existance($product['id_product'], $price_amount);
+
+                    if (empty($existing)) {
+
+                        if($insert_q != ""){
+                            $insert_q .= ',';
+                        }
+                        $insert_q .= '(' . $product['id_product'] . ',0,0,0,0,' . $price_amount . ',0,"' . date('Y-m-d') . '",' . $shop_id . ',' . $lang_id . ')';
+                    }
+                }
+            }
+        }
+
+        if($insert_q != "") {
+            $insert_q = "INSERT INTO `" . _DB_PREFIX_ . "omniversepricing_products` (`product_id`, `id_product_attribute`, `id_country`, `id_currency`, `id_group`, `price`, `promo`, `date`, `shop_id`, `lang_id`) VALUES $insert_q";
+        }
+        echo '<pre>';
+        print_r($insert_q);
+        echo '</pre>';
+        echo __FILE__ . ' : ' . __LINE__;
+        die(__FILE__ . ' : ' . __LINE__);
+    }
+
+    /**
+     * Check if price is alredy available for the product
+     */
+    private function check_existance($prd_id, $price, $id_attr = 0, $country = 0, $currency = 0, $group = 0)
+    {
+        $context = Context::getContext();
+        $lang_id = $context->language->id;
+        $shop_id = $context->shop->id;
+        $attr_q = '';
+        $curre_q = '';
+        $countr_q = '';
+        $group_q = '';
+
+        $attr_q = ' AND oc.`id_product_attribute` = ' . $id_attr;
+        $curre_q = ' AND oc.`id_currency` = ' . $currency;
+        $countr_q = ' AND oc.`id_country` = ' . $country;
+        $countr_q = ' AND oc.`id_country` = ' . $group;
+        $results = Db::getInstance()->executeS(
+            'SELECT *
+            FROM `' . _DB_PREFIX_ . 'omniversepricing_products` oc
+            WHERE oc.`lang_id` = ' . (int) $lang_id . ' AND oc.`shop_id` = ' . (int) $shop_id . '
+            AND oc.`product_id` = ' . (int) $prd_id . ' AND oc.`price` = ' . $price . $attr_q . $curre_q . $countr_q . $group_q
+        );
+
+        return $results;
+    }
 }
---
+++ controllers/front/sync.php
@@ -0,0 +1,15 @@
+<?php
+
+//http://localhost/prestashop/presta-7.8.7-addons/module/omniversepricing/sync
+
+class OmniversepricingSyncModuleFrontController extends ModuleFrontController
+{
+
+    public function initContent(){
+        
+
+
+    }
+
+
+}
