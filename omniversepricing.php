d8d8894352fba2987962de13bba006271d313041
TheEnumbin <enumbin@gmail.com>
2024-01-22 18:53:48 +0600

    Submitted 1.0.10

--- omniversepricing.php
+++ omniversepricing.php
@@ -35,7 +35,7 @@ class Omniversepricing extends Module
     public function __construct()
     {
         $this->name = 'omniversepricing';
-        $this->version = '1.0.9';
+        $this->version = '1.0.10';
         $this->tab = 'pricing_promotion';
         $this->author = 'TheEnumbin';
         $this->need_instance = 0;
