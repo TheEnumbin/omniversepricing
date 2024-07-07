384c0d886ed5f8da155e8391ab12e5f5d521503a
TheEnumbin <66176129+TheEnumbin@users.noreply.github.com>
2024-07-07 21:58:45 +0600

    remove check function from ajac controller

--- controllers/admin/AdminAjaxOmniverseController.php
+++ controllers/admin/AdminAjaxOmniverseController.php
@@ -301,32 +301,6 @@ class AdminAjaxOmniverseController extends ModuleAdminController
     /**
      * Check if price is alredy available for the product
      */
-    private function check_existance($prd_id, $lang_id, $price, $id_attr = 0, $country = 0, $currency = 0, $group = 0)
-    {
-        $context = Context::getContext();
-        $shop_id = $context->shop->id;
-        $attr_q = '';
-        $curre_q = '';
-        $countr_q = '';
-        $group_q = '';
-
-        if (!$id_attr) {
-            $id_attr = 0;
-        }
-        $attr_q = ' AND oc.`id_product_attribute` = ' . (int) $id_attr;
-        $curre_q = ' AND oc.`id_currency` = ' . (int) $currency;
-        $countr_q = ' AND oc.`id_country` = ' . (int) $country;
-        $group_q = ' AND oc.`id_group` = ' . (int) $group;
-        $results = Db::getInstance()->executeS(
-            'SELECT *
-            FROM `' . _DB_PREFIX_ . 'omniversepricing_products` oc
-            WHERE oc.`lang_id` = ' . (int) $lang_id . ' AND oc.`shop_id` = ' . (int) $shop_id . '
-            AND oc.`product_id` = ' . (int) $prd_id . ' AND oc.`price` = ' . $price . $attr_q . $curre_q . $countr_q . $group_q
-        );
-
-        return $results;
-    }
-
     private function getProductAttributesInfo($id_product, $shop_only = false)
     {
         return Db::getInstance()->executeS('
